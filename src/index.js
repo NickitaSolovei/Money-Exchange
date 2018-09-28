a60b7c6017914a8bdc6ce582cd53b7432b2d310a
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-09-28 13:11:40 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,50 +2,42 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-    var cur = currency;
-
-    var h = 50;
-    var q = 25;
-    var d = 10;
-    var n = 5;
-    var p = 1;
-
-    var obj1 = {};
+    let cur = currency;
+    const h = 50;
+    const q = 25;
+    const d = 10;
+    const n = 5;
+    const p = 1;
+    let obj = {};
 
     if (cur > 10000){
-        obj1.error = "You are rich, my friend! We don't have so much coins for exchange";
-        return obj1;
+        obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return obj;
     }
-
-    var numH = Math.floor(cur / h); 
+    const numH = Math.floor(cur / h); 
     if (numH >= 1) {
-        obj1.H = numH;
+        obj.H = numH;
         cur %= h; 
     }
-    
-    var numQ = Math.floor(cur / q);
+    const numQ = Math.floor(cur / q);
     if (numQ >= 1) {
-        obj1.Q = numQ;
+        obj.Q = numQ;
         cur %= q; 
     } 
-
-    var numD = Math.floor(cur / d);
+    const numD = Math.floor(cur / d);
     if (numD >= 1) {
-        obj1.D = numD;
+        obj.D = numD;
         cur %= d; 
     } 
-
-    var numN = Math.floor(cur / n);
+    const numN = Math.floor(cur / n);
     if (numN >= 1) {
-        obj1.N = numN;
+        obj.N = numN;
         cur %= n; 
     } 
-
-    var numP = Math.floor(cur / p);
+    const numP = Math.floor(cur / p);
     if (numP >= 1) {
-        obj1.P = numP;
+        obj.P = numP;
         cur %= p; 
     } 
-
-    return obj1;
+    return obj;
 }
